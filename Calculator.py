e07087843608034789c39f039033dad3824ac478
robot-learner <122472267+robot-learner@users.noreply.github.com>
2023-02-14 17:05:07 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -18,19 +18,22 @@ print("4. Divide")
 
 choice = input("Enter choice(1/2/3/4): ")
 
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-if choice == '1':
-    print(num1, "+", num2, "=", add(num1, num2))
+if choice >= '5':
+    print("Invalid input")
+    quit()
 
 elif choice == '2':
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
     print(num1, "-", num2, "=", subtract(num1, num2))
 
 elif choice == '3':
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
     print(num1, "*", num2, "=", multiply(num1, num2))
 
 elif choice == '4':
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
     print(num1, "/", num2, "=", divide(num1, num2))
-else:
-    print("Invalid input")
+
